80236c1d9433015e346bf1854135fee64e0296ff
Amritesh21 <amritesh999singh@gmail.com>
2023-09-08 20:21:11 +0530

    db object config changes

--- serverSide/src/main/java/com/tshirtShop/serverSide/ServerSideApplication.java
+++ serverSide/src/main/java/com/tshirtShop/serverSide/ServerSideApplication.java
@@ -1,12 +1,27 @@
 package com.tshirtShop.serverSide;
 
+import com.tshirtShop.serverSide.security.config.EnvironmentConfig;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ServerSideApplication {
-
 	public static void main(String[] args) {
+//		ConfigurableApplicationContext context = SpringApplication.run(ServerSideApplication.class, args);
+//		Scanner scanner = new Scanner(System.in);
+//		String enteredURL = scanner.nextLine();
+//		String dbURLVar = System.getenv("DB_URL");
+//		dbURLVar = enteredURL;
+//		System.out.println(System.getenv("DB_URL"));
 		SpringApplication.run(ServerSideApplication.class, args);
 	}
 
